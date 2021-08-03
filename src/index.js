0d1f93327b888dc3f08a3b97fb9038bbc2ad9353
Gabriel Mauas <gabymauas02@gmail.com>
2021-08-03 18:55:54 -0300

    Added react-router and Chakra-ui

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { ChakraProvider } from '@chakra-ui/react';
+import { BrowserRouter } from 'react-router-dom';
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ChakraProvider>
-        <App />
-    </ChakraProvider>
+    <BrowserRouter>
+      <ColorModeScript />
+      <ChakraProvider>
+          <App />
+      </ChakraProvider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
