1822ac62fb54bff2321c699d8ac7f435c41c1715
Gabriel Mauas <gabymauas02@gmail.com>
2021-11-02 17:25:38 -0300

    commit

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -3,23 +3,28 @@ import { getFirestore, collection } from "firebase/firestore";
 import { getAuth } from 'firebase/auth';
 
 
+const isProduction = () => {
+  if(process.env.NODE_ENV === 'production') {
+    return true
+  } else {
+    return false
+  }
+}
 
-console.log(process.env.REACT_APP_TASKING_FB_MODE);
+const firebaseApp = initializeApp({
+apiKey: isProduction() ? process.env.REACT_APP_TASKING_FB_API_KEY_PROD : process.env.REACT_APP_TASKING_FB_API_KEY,
+authDomain: isProduction() ? process.env.REACT_APP_TASKING_FB_AUTH_DOMAIN_PROD : process.env.REACT_APP_TASKING_FB_AUTH_DOMAIN,
+projectId: isProduction() ? process.env.REACT_APP_TASKING_FB_PROJECT_ID_PROD : process.env.REACT_APP_TASKING_FB_PROJECT_ID,
+storageBucket: isProduction() ? process.env.REACT_APP_TASKING_FB_STORAGE_BUCKET_PROD : process.env.REACT_APP_TASKING_FB_STORAGE_BUCKET,
+messagingSenderId: isProduction() ? process.env.REACT_APP_TASKING_FB_MESSAGING_SENDER_ID_PROD : process.env.REACT_APP_TASKING_FB_MESSAGING_SENDER_ID,
+appId: isProduction() ? process.env.REACT_APP_TASKING_FB_APP_ID_PROD : process.env.REACT_APP_TASKING_FB_APP_ID
+})
 
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_TASKING_FB_API_KEY,
-  authDomain: process.env.REACT_APP_TASKING_FB_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_TASKING_FB_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_TASKING_FB_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_TASKING_FB_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_TASKING_FB_APP_ID
-};
 
-console.log(firebaseConfig.apiKey);
 
 
 // Initialize Firebase
-const firebaseApp = initializeApp(firebaseConfig);
+// const firebaseApp = initializeApp(firebaseConfig);
 export const db = getFirestore(firebaseApp);
 export const foldersRef = collection(db, 'folders');
  
